41f2ab63e2e473fe30e237d2aefce87b36175c26
Chizo Nwazuo <chizonwazuo@yahoo.com>
2021-09-09 16:34:20 +0000

    [chore] 🔧 📦 add and configure twin macro

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        green: {
+          lighter: '#DEF5F2',
+          light: '#63BDAF',
+          medium: '#44A798',
+          DEFAULT: '#006156',
+          faint: '#C8E2DE',
+        },
+
+        yellow: { light: '#FFEEA7', DEFAULT: '#FFCE00' },
+
+        gray: {
+          darker: '#444854',
+          dark: '#575A65',
+          medium: ' #9DA8B6',
+          inactive: '#979AA5',
+          light: '#F9FAFC',
+          lighter: '#EFF2F7',
+        },
+        red: { DEFAULT: '#CC4945' },
+      },
+    },
+  },
+  plugins: [],
+};
