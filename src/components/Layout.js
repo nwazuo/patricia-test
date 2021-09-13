87d6201af1ea82cdc489f9a8a81823ab1fcbf9ea
Chizo Nwazuo <chizonwazuo@yahoo.com>
2021-09-13 13:20:37 +0000

    [doc] 💄 wallet card

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,9 +2,12 @@ import tw from 'twin.macro';
 import React from 'react';
 import { Sidebar } from '../components';
 
+import '../styles/layout.css';
+
 import { ReactComponent as CircledChevronIcon } from '../assets/circled-chevron.svg';
 import { ReactComponent as NairaBadgeDark } from '../assets/naira-badge-dark.svg';
 import walletPattern from '../assets/wallet-pattern.svg';
+import graph from '../assets/money-graph.svg';
 
 const LayeredCard = ({ children, adStyle }) => (
   // wheew, one of those moments where using tailwind is actually stressful
@@ -37,9 +40,27 @@ const Layout = ({ children }) => (
     `}
   >
     <Sidebar />
-    <div tw="block lg:flex flex-grow px-4">
+    <div
+      tw="block lg:flex flex-grow "
+      css={`
+        @media (min-width: 1024px) and (max-width: 1280px) {
+          justify-content: center;
+          flex-wrap: wrap;
+        }
+      `}
+    >
       {/* I use id's here to indicate sections because I don't have time to make components out of these. */}
-      <div id="wallet" tw="pt-4 lg:pt-10">
+      <div
+        id="wallet"
+        tw="px-4 pt-4 lg:pt-10"
+        css={`
+          flex-grow: 1;
+
+          @media (min-width: 1024px) {
+            max-width: 684px;
+          }
+        `}
+      >
         <div id="wallet-head">
           {/* I'm not sure why the medium font size isn't working, tried to fix it to no avail.  */}
           <h2 tw="text-gray-darker font-medium text-2xl lg:text-3xl">Wallet</h2>
@@ -60,22 +81,27 @@ const Layout = ({ children }) => (
             </div>
           </div>
         </div>
-        <div id="wallet-metric" tw="mt-5 lg:mt-6">
-          <LayeredCard adStyle={[`background: #006156; display: flex;`]}>
+        <div id="wallet-metric" tw="mt-5 lg:mt-6 mb-5 lg:mb-6">
+          <LayeredCard
+            adStyle={[
+              `background: #006156; display: flex; position: relative; overflow-x: scroll; -ms-overflow-style: none; scrollbar-width: none;`,
+              `&::-webkit-scrollbar {display: none};`,
+            ]}
+          >
             <div
               id="wallet-figures-wrapper"
               css={`
                 background-image: url(${walletPattern});
                 background-repeat: no-repeat;
               `}
-              tw="py-4 lg:py-6 pl-10"
+              tw="py-4 lg:py-6 pl-7 lg:pl-10"
             >
               <div
                 id="wallet-figures"
                 css={`
                   border-right: 1px solid #388379;
                 `}
-                tw="flex flex-col pr-5 lg:pr-8"
+                tw="flex flex-col pr-5 lg:pr-8 h-full justify-center"
               >
                 <p
                   tw="mb-3 lg:mb-4 font-semibold"
@@ -88,7 +114,7 @@ const Layout = ({ children }) => (
                 </p>
                 <p tw="text-white text-3xl">₦89,153,001.73</p>
                 <p
-                  tw="font-semibold mt-2"
+                  tw="font-semibold mt-1"
                   css={`
                     color: #c8e2de;
                     font-size: 14px;
@@ -97,6 +123,44 @@ const Layout = ({ children }) => (
                   Book Balance: ₦450,000.093
                 </p>
               </div>
+            </div>
+            <div id="money-graph" tw="flex flex-col py-3 lg:py-4 ml-5 lg:ml-8">
+              <div
+                id="intervals"
+                className="intervals"
+                tw="flex justify-between items-center"
+              >
+                {/* Some of the styling here was moved to a stylesheet to avoid excess duplication */}
+                <span>Today</span>
+                <span>Week</span>
+                <span>Month</span>
+                <span className="selected">Year</span>
+              </div>
+              <div id="graph" tw="my-3 lg:my-5">
+                {/* TODO: replace this with actual graph implementation */}
+                <img src={graph} alt="money graph" />
+              </div>
+              <div
+                id="sub-intervals"
+                className="intervals"
+                tw="flex justify-between"
+              >
+                <span>Jan</span>
+                <span>Apr</span>
+                <span>Jul</span>
+                <span>Oct</span>
+              </div>
+            </div>
+            <div
+              id="inflow-outflow"
+              tw="flex flex-col justify-center ml-3 lg:ml-5 mr-5 lg:mr-8"
+            >
+              <p tw="text-xs text-green-light font-medium">Outflow</p>
+              <p tw="text-xs text-green-faint font-bold">₦20,000</p>
+              <p tw="text-xs text-green-light font-medium mt-3 lg:mt-4">
+                Inflow
+              </p>
+              <p tw="text-xs text-green-faint font-bold">₦20,000</p>
             </div>
           </LayeredCard>
         </div>
