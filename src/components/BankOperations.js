6ccacb1a5ece23c0aef6475ba56d964309420def
Chizo Nwazuo <chizonwazuo@yahoo.com>
2021-09-18 23:08:43 +0000

    💄 bank operations UI progress

--- src/components/BankOperations.js
+++ src/components/BankOperations.js
@@ -1,11 +1,16 @@
 import tw from 'twin.macro';
-import React from 'react';
+import React, { useState } from 'react';
 import { Profile } from '../components';
 
 const BankOperations = () => {
+  const [shown, setShown] = useState(0);
+
+  const handleSwitch = (index) => {
+    setShown(index);
+  };
   return (
     <div
-      tw="flex-grow px-4 pt-4 lg:pt-4 pb-4 lg:pb-6 "
+      tw="flex-grow px-4 lg:pt-4 pb-4 lg:pb-6 "
       css={`
         @media (min-width: 1024px) {
           max-width: 684px;
@@ -14,7 +19,10 @@ const BankOperations = () => {
           border-left: 1px solid #EFF2F7;
           margin-top: 24px;
           margin-bottom: 24px;
-          padding-right: 32px;
+          padding-left: 24px;
+          padding-right: 24px;
+          width: calc(428px + 48px);
+          flex-grow: 0;
 }
         }
       `}
@@ -29,7 +37,56 @@ const BankOperations = () => {
       >
         <Profile />
       </div>
+      <div
+        id="bank-operations-card"
+        tw="rounded-3xl border border-gray-lighter"
+        css={`
+          @media (min-width: 1280px) {
+            margin-top: 62px;
+          }
+        `}
+      >
+        <div id="tabs" tw="flex">
+          <TabSelect index={0} shown={shown} switchTab={handleSwitch}>
+            Deposit
+          </TabSelect>
+          <TabSelect index={1} shown={shown} switchTab={handleSwitch}>
+            Transfer
+          </TabSelect>
+        </div>
+        <TabDisplay index={0} shown={shown}>
+          <div
+            tw="pt-6 lg:pt-9 mx-auto text-center font-bold text-gray-darker text-sm lg:text-lg"
+            css={`
+              max-width: 237px;
+            `}
+          >
+            Select a method to Fund Naira wallet below
+          </div>
+        </TabDisplay>
+      </div>
     </div>
+  );
+};
+
+const TabDisplay = ({ index, shown, children, switchTab }) => {
+  return <div css={[tw`hidden`, index === shown && tw`block`]}>{children}</div>;
+};
+
+const TabSelect = ({ index, shown, children, switchTab }) => {
+  const inactive = tw`text-gray-medium bg-gray-lighter`;
+
+  return (
+    <span
+      tw="py-5 text-green text-center flex-1 cursor-pointer"
+      css={[
+        index !== shown && inactive,
+        `&:first-child {border-top-left-radius: 24px;} &:last-child {border-top-right-radius: 24px;}`,
+      ]}
+      onClick={() => switchTab(index)}
+    >
+      {children}
+    </span>
   );
 };
 
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -61,134 +61,140 @@ const Layout = ({ children }) => (
       `}
     >
       {/* I use id's here to indicate sections because I don't have time to make components out of these, yet. */}
-      <div
-        id="wallet"
-        tw="px-4 pt-4 lg:pt-10 pb-4 lg:pb-6"
-        css={`
-          flex-grow: 1;
-
-          @media (min-width: 1024px) {
-            max-width: 684px;
-          }
-        `}
-      >
-        <div id="wallet-head">
-          {/* I'm not sure why the medium font size isn't working, tried to fix it to no avail.  */}
-          <h2 tw="text-gray-darker font-medium text-2xl lg:text-3xl">Wallet</h2>
-          <div id="wallet-current" tw="flex mt-3 lg:mt-4">
-            <NairaBadgeDark tw="mr-4" />
-            <div>
-              <h3 tw="flex items-center font-medium text-xl text-gray-darker leading-6 cursor-pointer">
-                Naira Wallet <CircledChevronIcon tw="ml-2" />
-              </h3>
-              <p
-                tw="font-bold text-gray-medium"
-                css={`
-                  font-size: 14px;
-                `}
-              >
-                Lorem Ipsum dolor sit amet
-              </p>
+      <div id="wallet-wrap" tw="flex-grow">
+        <div
+          id="wallet"
+          tw="px-4 pt-4 lg:pt-10 pb-4 lg:pb-6 mx-auto"
+          css={`
+            flex-grow: 1;
+            @media (min-width: 1024px) {
+              max-width: 684px;
+            }
+          `}
+        >
+          <div id="wallet-head">
+            {/* I'm not sure why the medium font size isn't working, tried to fix it to no avail.  */}
+            <h2 tw="text-gray-darker font-medium text-2xl lg:text-3xl">
+              Wallet
+            </h2>
+            <div id="wallet-current" tw="flex mt-3 lg:mt-4">
+              <NairaBadgeDark tw="mr-4" />
+              <div>
+                <h3 tw="flex items-center font-medium text-xl text-gray-darker leading-6 cursor-pointer">
+                  Naira Wallet <CircledChevronIcon tw="ml-2" />
+                </h3>
+                <p
+                  tw="font-bold text-gray-medium"
+                  css={`
+                    font-size: 14px;
+                  `}
+                >
+                  Lorem Ipsum dolor sit amet
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-        <div id="wallet-metric" tw="mt-5 lg:mt-6 mb-5 lg:mb-6">
-          <LayeredCard
-            adStyle={[
-              `background: #006156; display: flex; justify-content: space-between; position: relative; overflow-x: scroll; -ms-overflow-style: none; scrollbar-width: none;`,
-              `&::-webkit-scrollbar {display: none};`,
-            ]}
-          >
-            <div
-              id="wallet-figures-wrapper"
-              css={`
-                background-image: url(${walletPattern});
-                background-repeat: no-repeat;
-              `}
-              tw="py-4 lg:py-6 pl-7 lg:pl-10"
+          <div id="wallet-metric" tw="mt-5 lg:mt-6 mb-5 lg:mb-6">
+            <LayeredCard
+              adStyle={[
+                `background: #006156; display: flex; justify-content: space-between; position: relative; overflow-x: scroll; -ms-overflow-style: none; scrollbar-width: none;`,
+                `&::-webkit-scrollbar {display: none};`,
+              ]}
             >
               <div
-                id="wallet-figures"
+                id="wallet-figures-wrapper"
                 css={`
-                  border-right: 1px solid #388379;
+                  background-image: url(${walletPattern});
+                  background-repeat: no-repeat;
                 `}
-                tw="flex flex-col pr-5 lg:pr-8 h-full justify-center"
+                tw="py-4 lg:py-6 pl-7 lg:pl-10"
               >
-                <p
-                  tw="mb-3 lg:mb-4 font-semibold"
+                <div
+                  id="wallet-figures"
                   css={`
-                    color: #63bdaf;
-                    font-size: 14px;
+                    border-right: 1px solid #388379;
                   `}
+                  tw="flex flex-col pr-5 lg:pr-8 h-full justify-center"
                 >
-                  Total Balance
-                </p>
-                <p tw="text-white text-3xl">₦89,153,001.73</p>
-                <p
-                  tw="font-semibold mt-1"
-                  css={`
-                    color: #c8e2de;
-                    font-size: 14px;
-                  `}
-                >
-                  Book Balance: ₦450,000.093
-                </p>
-              </div>
-            </div>
-            <div id="money-graph" tw="flex flex-col py-3 lg:py-4 ml-5 lg:ml-8">
-              <div
-                id="intervals"
-                className="intervals"
-                tw="flex justify-between items-center"
-              >
-                {/* Some of the styling here was moved to a stylesheet to avoid excess duplication */}
-                <span>Today</span>
-                <span>Week</span>
-                <span>Month</span>
-                <span className="selected">Year</span>
-              </div>
-              <div id="graph" tw="my-3 lg:my-5">
-                {/* TODO: replace this with actual graph implementation using rechartsJS line graph */}
-                <img src={graph} alt="money graph" />
+                  <p
+                    tw="mb-3 lg:mb-4 font-semibold"
+                    css={`
+                      color: #63bdaf;
+                      font-size: 14px;
+                    `}
+                  >
+                    Total Balance
+                  </p>
+                  <p tw="text-white text-3xl">₦89,153,001.73</p>
+                  <p
+                    tw="font-semibold mt-1"
+                    css={`
+                      color: #c8e2de;
+                      font-size: 14px;
+                    `}
+                  >
+                    Book Balance: ₦450,000.093
+                  </p>
+                </div>
               </div>
               <div
-                id="sub-intervals"
-                className="intervals"
-                tw="flex justify-between"
+                id="money-graph"
+                tw="flex flex-col py-3 lg:py-4 ml-5 lg:ml-8"
               >
-                <span>Jan</span>
-                <span>Apr</span>
-                <span>Jul</span>
-                <span>Oct</span>
+                <div
+                  id="intervals"
+                  className="intervals"
+                  tw="flex justify-between items-center"
+                >
+                  {/* Some of the styling here was moved to a stylesheet to avoid excess duplication */}
+                  <span>Today</span>
+                  <span>Week</span>
+                  <span>Month</span>
+                  <span className="selected">Year</span>
+                </div>
+                <div id="graph" tw="my-3 lg:my-5">
+                  {/* TODO: replace this with actual graph implementation using rechartsJS line graph */}
+                  <img src={graph} alt="money graph" />
+                </div>
+                <div
+                  id="sub-intervals"
+                  className="intervals"
+                  tw="flex justify-between"
+                >
+                  <span>Jan</span>
+                  <span>Apr</span>
+                  <span>Jul</span>
+                  <span>Oct</span>
+                </div>
               </div>
-            </div>
-            <div
-              id="inflow-outflow"
-              tw="flex flex-col justify-center ml-3 lg:ml-5 mr-5 lg:mr-8"
-            >
-              <p tw="text-xs text-green-light font-medium">Outflow</p>
-              <p tw="text-xs text-green-faint font-bold">₦20,000</p>
-              <p tw="text-xs text-green-light font-medium mt-3 lg:mt-4">
-                Inflow
-              </p>
-              <p tw="text-xs text-green-faint font-bold">₦20,000</p>
-            </div>
-          </LayeredCard>
+              <div
+                id="inflow-outflow"
+                tw="flex flex-col justify-center ml-3 lg:ml-5 mr-5 lg:mr-8"
+              >
+                <p tw="text-xs text-green-light font-medium">Outflow</p>
+                <p tw="text-xs text-green-faint font-bold">₦20,000</p>
+                <p tw="text-xs text-green-light font-medium mt-3 lg:mt-4">
+                  Inflow
+                </p>
+                <p tw="text-xs text-green-faint font-bold">₦20,000</p>
+              </div>
+            </LayeredCard>
+          </div>
+          <div
+            id="card-actions"
+            css={`
+              grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+            `}
+            tw="grid gap-4 lg:gap-6"
+          >
+            <CardButton text="Request Money" icon={<RequestMoneyIcon />} />
+            <CardButton
+              text="Cardless Withdrawal"
+              icon={<CardlessWithdrawalIcon />}
+            />
+          </div>
+          <RecentActivity />
         </div>
-        <div
-          id="card-actions"
-          css={`
-            grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-          `}
-          tw="grid gap-4 lg:gap-6"
-        >
-          <CardButton text="Request Money" icon={<RequestMoneyIcon />} />
-          <CardButton
-            text="Cardless Withdrawal"
-            icon={<CardlessWithdrawalIcon />}
-          />
-        </div>
-        <RecentActivity />
       </div>
       <FlexBreak />
       <BankOperations />
